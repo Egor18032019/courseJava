d2f09bc2605fbc44133defaaea57a7fa3e2c3830
Egor18032019 <goro4@mail.ru>
2021-07-21 10:07:18 +0500

    закончил работу над домашкой

--- src/lesson7/HomeWorkLesson7Threading.java
+++ src/lesson7/HomeWorkLesson7Threading.java
@@ -23,22 +23,34 @@ public class HomeWorkLesson7Threading {
         // +  в машинку передаем защёлку и вызываем её внутри класса
         for (int i = 0; i < CARS_COUNT; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cyclicBarrier,lockFinish);
-        }
-
         // тут мы должны в каждый поток передать машинку которая едет по трасе
         // (расчет трасы в классе Car)
-        for (int i = 0; i < CARS_COUNT; i++) {
             Thread thread = new Thread(cars[i]);
             thread.start();
             threadsArray[i] = thread;
         }
-        cyclicBarrier.await();
-        // что бы когда защелка cyclicBarrier упал
-        // и последний закончивший не стартанул до обьявления
-        // в Car ставим проверку на барьер
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+        try {
+            cyclicBarrier.await();
+            // что бы когда защелка cyclicBarrier упал
+            // и последний закончивший не стартанул до обьявления
+            // в Car ставим проверку на барьер
+            //  и это не помогает
+            // ставим после обьявления еще один барьер
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            cyclicBarrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
         for (int i = 0; i < CARS_COUNT; i++)
+            try {
             threadsArray[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 
