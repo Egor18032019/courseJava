360760dba29570f6a838403fc3eded1e134cdc3c
Egor18032019 <goro4@mail.ru>
2021-07-22 15:56:44 +0500

    домашка по 8 лекции

--- src/lesson8/homeWork/MainApp.java
+++ src/lesson8/homeWork/MainApp.java
@@ -4,16 +4,45 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
+/*
+Создайте массив объектов типа Сотрудник (с полями Имя, Возраст, Зарплата) и вычислите
+среднюю зарплату сотрудника;
+3. Напишите метод, способный найти в массиве сотрудников из п. 2 найдите N самых старших
+сотрудников и отпечатает в консоль сообщение вида “N самых старших сотрудников зовут:
+имя1, имя2, имяN;”.
+ */
 public class MainApp {
-    public static void main(String[] args) {
-        HashMapForWords WordsMap = new HashMapForWords();
+    private static String path;
+    final static int LIMIT = 3;
 
-        Optional<String> occurringWord = null;
+    public static void main(String[] args) {
+        String filePath = "c:\\Users\\Teacher\\Documents\\GitHub\\JAVA\\src\\lesson8\\homeWork\\text.txt";
+
+        Person[] persons = {
+                new Person("V1bo", 22, 1, Position.MANAGER),
+                new Person("Bob2", 44, 1, Position.DIRECTOR),
+                new Person("Bob3", 25, 1, Position.ENGINEER),
+                new Person("Bob4", 42, 1, Position.ENGINEER),
+                new Person("Bob5", 55, 1, Position.MANAGER),
+                new Person("Bob6", 19, 1, Position.MANAGER),
+                new Person("Bob7", 33, 1, Position.ENGINEER),
+                new Person("Bob8", 37, 1, Position.MANAGER)
+        };
+        System.out.println(getFrequentlyOccurringWord(filePath));
+        System.out.println(getAverageSalary(persons));
+        System.out.println(getOldName(persons));
+    }
+
+
+    public static String getFrequentlyOccurringWord(String path) {
+        MainApp.path = path;
+        String answer;
+        HashMapForWords WordsMap = new HashMapForWords();
+        Optional<String> occurringWord = Optional.empty();
         try {
-            occurringWord = Files.lines(Paths.get("c:\\Users\\Teacher\\Documents\\GitHub\\JAVA\\src\\lesson8\\homeWork\\text.txt"))
+            occurringWord = Files.lines(Paths.get(path))
                     .map(everyLine -> everyLine.split("\\s"))
                     .flatMap(Arrays::stream)
                     // дальше складываю всё в новый HashMap WordsMap .
@@ -33,39 +62,31 @@ public class MainApp {
 
         if (WordsMap.get((occurringWord.orElse(null))) > 1) {
             // и распечатываем со значением по умолчанию
-            System.out.println("Часто встречающееся слово - " + occurringWord.orElse("Что то пошло не так"));
+            answer = "Часто встречающееся слово - " + occurringWord.orElse("Что то пошло не так");
         } else {
-            System.out.println("Нет повторяющихся");
+            answer = "Нет повторяющихся";
         }
+        return answer;
     }
 
-    static class HashMapForWords extends HashMap<String, Integer> {
-        public void put(String key) {
-            // если не было то ложим со значением 1
-            if (!containsKey(key)) {
-                put(key, 1);
-                return;
-            } else {
-                // в другом случаем увеличивам
-                put(key, get(key) + 1);
-            }
+    public static String getAverageSalary(Person[] arr) {
+        String answer = "Нет данных";
+        if (arr.length > 0) {
+            Optional<Integer> averageSalary = Arrays.stream(arr)
+                    .map(s -> s.getSalary())
+                    .reduce((acc, per) -> acc + per);
+            answer = "Средняя зп = " + averageSalary.orElse(0) / arr.length;
         }
-
+        return answer;
     }
 
-    public static String getCountWords(String[] arr) {
-        Map<String, Integer> mapForwords = new HashMap<>();
-        Integer firstTimeWords = 1;
-        for (String it : arr) {
-            if (!mapForwords.containsKey(it)) {
-                mapForwords.put(it, firstTimeWords);
-            } else {
-                mapForwords.put(it, mapForwords.get(it) + 1);
-            }
-        }
-
-        String mostUnick = Collections.max(mapForwords.entrySet(), (entry1, entry2) -> entry1.getValue() - entry2.getValue()).getKey();
-
-        return mostUnick;
+    public static String getOldName(Person[] arr) {
+        String message = Arrays.stream(arr)
+                .sorted((o1, o2) -> o2.getAge() - o1.getAge())
+                .limit(LIMIT)
+                .map(s -> s.getName())
+                .collect(Collectors.joining(" ,", LIMIT + " самых старших сотрудников зовут: ", "."));
+        return message;
     }
 }
+
